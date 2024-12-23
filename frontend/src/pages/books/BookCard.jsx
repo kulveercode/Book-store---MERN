18a5089c2018217c5973dd44d2f825ddf99f4e27
Kulveer Singh <kulveer.coder@gmail.com>
2024-12-23 15:17:49 +0530

    orders page updated

--- frontend/src/pages/books/BookCard.jsx
+++ frontend/src/pages/books/BookCard.jsx
@@ -28,7 +28,7 @@ const BookCard = ({ book }) => {
               {book?.title}
             </h3>
           </Link>
-          <p className="text-gray-600 mb-5 text-sm">{book?.description.length > 60 ? `${book?.description.slice(0, 80)}...` : book?.description}</p>
+          <p className="text-gray-600 mb-5 text-sm">{book?.description.length > 40 ? `${book?.description.slice(0, 80)}...` : book?.description}</p>
           <p className="font-medium mb-5">
             ${book?.newPrice} <span className="line-through font-normal ml-2">${book?.oldPrice}</span>
           </p>
--- frontend/src/pages/books/OrderPage.jsx
+++ frontend/src/pages/books/OrderPage.jsx
@@ -20,8 +20,24 @@ const OrderPage = () => {
         ) : (
           <div>
             {orders.map((order, index) => (
-              <div key={index}>
-                <h3>Order id: {order?._id}</h3>
+              <div key={index} className="mb-4 border-b pb-2">
+                <p className="p-1 bg-secondary text-white w-10 rounded mb-1"># {index + 1}</p>
+                <h3 className="font-bold">Order id: {order?._id}</h3>
+                <p className="text-gray-600">Order Id: {order._id}</p>
+                <p className="text-gray-600">Name: {order.name}</p>
+                <p className="text-gray-600">Email: {order.email}</p>
+                <p className="text-gray-600">Phone: {order.phone}</p>
+                <p className="text-gray-600">Total Price: ${order.totalPrice}</p>
+                <p className="text-gray-600"></p>
+                <h3 className="font-medium">Address:</h3>
+                <p>{order.address.city}, {order.address.state}, {order.address.country}, {order.address.zipcode}</p>
+                <h3 className="font-medium">Product Id:</h3>
+                <ul>
+                  {order.productsIds.map((productId) => (
+                    <li key={productId}>{productId}</li>
+                  ))}
+                </ul>
+
               </div>
             ))}
           </div>
